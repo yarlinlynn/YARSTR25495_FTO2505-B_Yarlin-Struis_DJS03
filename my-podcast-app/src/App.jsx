5df8c4e0954f73aadae57286f05c2a3b166c5282
Yarlin <yarlinlynn@gmail.com>
2025-09-19 11:34:34 +0200

    Fetch podcasts from API using useEffect, logging them to the console with a 2s delay and set loading and error states using useState

--- my-podcast-app/src/App.jsx
+++ my-podcast-app/src/App.jsx
@@ -1,9 +1,54 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [podcasts, setPodcast] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null)
+
+  useEffect( () => {
+    const fetchPodcasts = async () => {
+      try {
+        const response = await fetch((`https://podcast-api.netlify.app/`));
+        if(!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`)
+        } 
+        const podcasts = await response.json();
+        setPodcast(podcasts)
+        console.log(podcasts)
+      } catch(error) {
+        setError(error)
+      } finally {
+        setLoading(false)
+      }
+    };
+
+    const delay = setTimeout( () => {
+      fetchPodcasts();
+    }, 2000);
+    return () => clearTimeout(delay)
+  }, [])
+
+  if(loading) {
+    return 
+    <div className='loading-container'>
+      <div>
+        
+        <div className="loading-animation"></div>
+        
+        <p className="loading-text">Fetching data... please wait.</p>
+      </div>
+    </div>
+  }
+
+  if(error) {
+    return
+    <div className="error-container">
+      <p className="error-message">
+        Failed to fetch tasks from API.
+      </p>
+    </div>
+  }
 
   return (
     <>
